91cbc2400f7b4a93e6de0e998a07a622c5b7b52f
mariavisual166 <mariavisual166@gmail.com>
2022-09-27 21:17:14 +0000

    test open gogle map

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,19 @@
-import React from "react";
-import MainPage from "../MainPage/MainPage";
+const App = () => {
+  const onClick = () => {
+    window.open(
+      "https://www.google.com/maps/search/?api=1&query=47.5951518,-122.3316393",
+      "_blank"
+    );
+  };
 
-function App() {
-  return <MainPage />;
-}
+  // Candidate must fill in implementation here
+  return (
+    <>
+      <div className="App">
+        <p onClick={onClick}>Click aqui</p>
+      </div>
+    </>
+  );
+};
 
 export default App;
